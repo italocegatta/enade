d8df087e9cdbbbb0b94feb6cdaf74ab39e27199b
italocegatta <italocegatta@gmail.com>
2018-07-07 20:01:07 -0300

    kick off

---
+++ R/run-enade-dashboard.R
@@ -0,0 +1,12 @@
+#' Run dashboard
+#'
+#' Carrega um dashboard com os prencipais resultados
+#' @export
+enade_dashboard <- function() {
+  appDir <- system.file("shiny", "dashboard", package = "enade")
+  if (appDir == "") {
+    stop("Could not find example directory. Try re-installing `mypackage`.", call. = FALSE)
+  }
+
+  shiny::runApp(appDir, display.mode = "normal")
+}
